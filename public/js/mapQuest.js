249e1c12abae155a5ec87483f0234b01eca2d0b5
Rebecca Hsiao <rebeccahsiao@Rebeccas-MacBook-Pro.local>
2020-02-27 12:13:02 -0800

    done

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+  L.mapquest.key = '2qjTYOixTkl3v82mlIF1R9tu0uE3C3UN';
+
+  var map = L.mapquest.map('map', {
+    center: [32.88, -117.236],
+    layers: L.mapquest.titleLayer('map'),
+    zoom: 12
+    zoomControl: false
+  });
+
+  L.marker([32.88, -117.236]).addTo(map);
 }
